d46b91948dfc7f7a626067d4676dacaebcb3a626
tzrph <tzrpheonix@gmail.com>
2025-04-16 15:08:44 +0200

    update vercel

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
   optimizeDeps: {
     include: ['vue']
